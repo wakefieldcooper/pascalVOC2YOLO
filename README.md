e32dfac968f14f82d7854c9f7824bca80d4359c1
Cooper Wakefield <enquiries@cooperwakefield.com>
2021-04-09 12:11:18 +1000

    Updated from old manifest file to individual yolo format

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# pascalvoc2yolov4txt
-Conversion tool for object detection tasks. Convert PascalVOC xml's to yolov4txt
+# pascalvoc2yolov5txt
+Conversion tool for object detection tasks. Convert PascalVOC xml's to yolov5txt
+
+
+## Install
+### Dependencies
+Python3, tested on 3.7.9, however should work on 3.6+
+
+Uses python package virtualenv, to install run the following:
+```pip install virtualenv```
+
+To create a virtualenv, run the following command in the root of the directory
+```virtualenv venv```
+
+To activate the virtualenv run:
+###### On Windows OS
+```venv\Scripts\activate```
+###### On Linux/Mac
+```source venv/bin/activate```
+
+To install dependencies, run the following command
+```pip install -r requirements.txt```
+
+To run the program, ensure all xmls are in a single directory you can reference, and you have created an empty output directory for the txt files. Then run
+```python app.py --input path/to/your/xmls --output path/to/where/you/want/your/txt/files```
+
+## Further information
+For information on dataset format and training information.
+Please refer to: [Caption]https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data
+This will cover how pixel dimensions are converted to normalised xywh format.
--- app.py
+++ app.py
@@ -1,47 +1,73 @@
 import os
 import json
 from lxml import etree
-from glob import iglob
+# from glob import iglob
 from pathlib import Path
 import argparse
 
+def convert(img_size, coords):
+    """Convert from xmin,xmax,ymin,ymax to xywh normalised format
+    Args:
+        img_size(array): array of (img_width, img_height)
+        coords(array): Array of class_id, xmin, xmax, ymin, ymax
+    Returns:
+        xywh(array): Array of normalised xywh coords
+    """
+    img_width = img_size[0]
+    img_height = img_size[1]
+    #Calculate Bounding box dimensions from xml
+    class_id = str(coords[0])
+    x_centre = str(((coords[1]+coords[2])/2)/img_width)
+    y_centre = str(((coords[3]+coords[4])/2)/img_height)
+    height = str((coords[4]-coords[3])/img_height)
+    width = str((coords[2]-coords[1])/img_width)
+    
+    xywh = (class_id, x_centre, y_centre, width, height)
+    return xywh
 def main():
+    """Main processing loop that converts pascal voc xmls to yolov5 txt files.
+    """
     labels = {}
     categories = {}
-    Path(args.input).mkdir(parents=True, exist_ok=True)
+    # Path(args.input).mkdir(parents=True, exist_ok=True)
     Path(args.output).mkdir(parents=True, exist_ok=True)
     ##NOTE: May need to ensure it doesn't overwrite existing. not needed 
-    #as functionality rn
-    with open(os.path.join(args.output, 'manifest.txt'), 'w') as txtfile:
-        for xml_file in os.scandir(args.input):
+    # as functionality right now. Onus on user to ensure original xml files
+    # don't have duplicates names.
+    for xml_file in os.scandir(args.input):
+        with open(os.path.join(args.output, f'{Path(xml_file).stem}.txt'), 'w') as txtfile:
             with open(xml_file.path) as file:
                 print(f'Processing file {xml_file.name}...')
                 #create annotations object
                 annotations = etree.fromstring(file.read())
-                #extract 
+                #extract elements that are needed 
                 image_filename = annotations.find('filename').text
+                image_size = annotations.find('size')
+                image_width = float(image_size.find('width').text)
+                image_height = float(image_size.find('height').text)
                 boxes = annotations.iterfind('object')
                 file_line = image_filename
                 for box in boxes:
                     annotation_list = []
+                    #Extract bounding box pixel data
                     bndbox = box.find('bndbox')
-                    xmin = str(bndbox.find('xmin').text)
-                    ymin = str(bndbox.find('ymin').text)
-                    xmax = str(bndbox.find('xmax').text)
-                    ymax = str(bndbox.find('ymax').text)
+                    xmin = float(bndbox.find('xmin').text)
+                    ymin = float(bndbox.find('ymin').text)
+                    xmax = float(bndbox.find('xmax').text)
+                    ymax = float(bndbox.find('ymax').text)
 
                     label_name = box.find('name').text
-
                     if label_name not in labels:
                         labels[label_name] = len(labels)
-
                     class_id = labels[label_name]
                     categories[class_id] = label_name
-                    annotation_list.extend((xmin, ymin, xmax, ymax, str(class_id)))
-                    line = ','.join(annotation_list)
-                    file_line = ' '.join((file_line, line))
-            txtfile.write(f'{file_line}\n')
-    txtfile.close()
+
+                    xywh = convert((image_width, image_height), (class_id, xmin, xmax, ymin, ymax))
+                    annotation_list.extend(xywh)
+                    line = ' '.join(annotation_list)
+                    # file_line = ' '.join((file_line, line))
+                    txtfile.write(f'{line}\n')
+        txtfile.close()
     print(categories)
 
 if __name__ == "__main__":
